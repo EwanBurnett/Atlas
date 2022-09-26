ace710ed3b05f3965ce0b65ac1f08a98367ea4b8
Ewan Burnett <EwanBurnettSK@outlook.com>
2022-09-26 06:24:08 +0100

    Git Submodules Built with projects

--- Examples/00-Hello-World/main.cpp
+++ Examples/00-Hello-World/main.cpp
@@ -1,10 +1,14 @@
 #include <cstdio>
 #include <Atlas.h>
 #include <iostream>
+#include <lua.h>
+#include <GLFW/glfw3.h>
+#include <AL/al.h>
+#include <tinyxml2.h>
+#include <assimp/scene.h>
 
 int main() {
     Atlas::Debug("Hello World!");
-
     printf("Debug");
     std::cin.get();
     return 0;
